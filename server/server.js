6e9ce34087c9ee417b727af910ed8a0ab3d398e6
Suraj Kumar <suraj@sunson.in>
2018-01-26 13:22:57 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+
+const publicdir = path.join(__dirname, "../public");
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicdir));
+
+app.listen(3000, (err) => { if (!err) { console.log(`Listening on port ${port}`); } else { console.log("Failed to acquire port 3000 for listening"); process.exit(1); } });
+
