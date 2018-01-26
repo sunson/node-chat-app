7e362d61ab180a70c75b717d025f6d2249ea403b
Suraj Kumar <suraj@sunson.in>
2018-01-26 13:49:56 +0530

    reindent

--- server/server.js
+++ server/server.js
@@ -9,5 +9,13 @@ var app = express();
 
 app.use(express.static(publicdir));
 
-app.listen(3000, (err) => { if (!err) { console.log(`Listening on port ${port}`); } else { console.log("Failed to acquire port 3000 for listening"); process.exit(1); } });
+app.listen(3000, (err) => {
+    if (!err) {
+	console.log(`Listening on port ${port}`);
+    } else {
+	console.log("Failed to acquire port 3000 for listening");
+	process.exit(1);
+    }
+});
+
 
