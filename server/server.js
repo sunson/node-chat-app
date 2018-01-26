e5d27e7ac72572844adb9335ec6255d95a9736a1
Suraj Kumar <suraj@sunson.in>
2018-01-26 13:54:53 +0530

    bugfix: use port variable while listen()ing

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
+var socketIO = require('socket.io');
 
 const publicdir = path.join(__dirname, "../public");
 
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
 
 app.use(express.static(publicdir));
 
-app.listen(3000, (err) => {
+server.listen(port, (err) => {
     if (!err) {
 	console.log(`Listening on port ${port}`);
     } else {
-	console.log("Failed to acquire port 3000 for listening");
+	console.log(`Failed to acquire port ${port} for listening`);
 	process.exit(1);
     }
 });
